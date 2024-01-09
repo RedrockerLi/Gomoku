0a7788c5da4c09db79a92cb749b756ae1d99bd05
RedrockerLi <redrocker3047@gmail.com>
2024-01-10 01:13:13 +0800

    update

--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
 #使用这个文件可以创建16个文件夹,同步使用粒子群算法
 #使用原因:如果初始条件不够好很容易陷入局部最优
+rm -rf Train
 mkdir Train
 cd Train
 count=1
@@ -8,5 +9,6 @@ do
 mkdir Train$count
 cp -p ../main ./Train$count
 touch ./Train$count/trainLog
+./main &
 let "count++"
 done
